8cb4806b405b6d975541e80f2a12df5817c067d9
l4511 <98616630+l4511@users.noreply.github.com>
2022-09-08 22:08:12 +0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = "5024084066:AAG4czr2tIKJI4rdyVVZgTxPrEa3JVypjZI"
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'лари': 'GEL',
+    'евро': 'EUR',
+}
---
+++ utils.py
@@ -0,0 +1,32 @@
+import json
+import requests
+from config import keys
+
+
+class APIExceptions(Exception):
+	pass
+
+
+class Convertor:
+	@staticmethod
+	def get_price(amount, base, quote):
+		try:
+			base_ticker = keys[base.lower()]
+		except KeyError:
+			raise APIExceptions(f'Валюта {base} не найдена!')
+		try:
+			quote_ticker = keys[quote.lower()]
+		except KeyError:
+			raise APIExceptions(f'Валюта {quote} не найдена!')
+		if base == quote:
+			raise APIExceptions(f'Невозможно перевести одинаковые валюты {quote}!')
+		try:
+			amount = float(amount)
+		except ValueError:
+			raise APIExceptions(f' Не удалось обработать количество {amount}!')
+
+		r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={base_ticker}&tsyms={quote_ticker}')
+		total_base = json.loads(r.content)[keys[quote]]
+		text = f'Цена {amount} {base}  в {quote}  : {total_base}'
+
+		return text
